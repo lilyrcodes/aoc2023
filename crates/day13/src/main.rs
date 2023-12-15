e651efdaf2fcd597938cb830602f44f01a99a621
Lily Richards <lilymichellerichards@gmail.com>
2023-12-14 22:13:54 -0500

    Outline of day 13

---
+++ crates/day13/src/main.rs
@@ -0,0 +1,110 @@
+use std::{collections::HashMap, fs::read_to_string};
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+enum Tile {
+    Rock,
+    Empty,
+}
+
+impl From<char> for Tile {
+    fn from(value: char) -> Self {
+        if value == '#' {
+            Tile::Rock
+        } else {
+            Tile::Empty
+        }
+    }
+}
+
+type Row = Vec<Tile>;
+
+fn make_row(s: &str) -> Row {
+    s.chars().map(Tile::from).collect()
+}
+
+type Map = Vec<Row>;
+
+fn make_maps(s: &str) -> Vec<Map> {
+    let mut maps = Vec::new();
+    let mut map = Map::new();
+    for line in s.lines() {
+        if line.is_empty() {
+            maps.push(map);
+            map = Map::new();
+        } else {
+            map.push(make_row(line));
+        }
+    }
+    if !map.is_empty() {
+        maps.push(map);
+    }
+    maps
+}
+
+fn is_palindrome_at(r: &Row, idx: usize) -> bool {
+    let (left, right) = r.split_at(idx);
+    left.iter().zip(right.iter().rev()).all(|(a, b)| a == b)
+}
+
+fn find_possible_horiz_points(r: &Row) -> Vec<usize> {
+    (1..r.len())
+        .filter(|idx| is_palindrome_at(r, *idx))
+        .collect()
+}
+
+fn find_possible_vert_points(m: &Map, idx: usize) -> Vec<usize> {
+    find_possible_horiz_points(&m.iter().map(|row| row[idx]).collect::<Row>())
+}
+
+fn calc_map_points(m: Map) -> usize {
+    todo!()
+}
+
+fn part1(s: &str) -> usize {
+    make_maps(s).into_iter().map(calc_map_points).sum()
+}
+
+fn part2(s: &str) -> usize {
+    todo!()
+}
+
+fn main() {
+    let input = read_to_string("input.txt").unwrap();
+    let answer1 = part1(&input);
+    println!("Part 1: {}", answer1);
+    let answer2 = part2(&input);
+    println!("Part 2: {}", answer2);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    const TEST_INPUT: &str = "#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#";
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(TEST_INPUT), 21);
+    }
+
+    /*
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(TEST_INPUT), 525152);
+    }
+    */
+}
