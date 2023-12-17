44c178ac62ad3b803920fefd2651c1f623459054
Lily Richards <lilymichellerichards@gmail.com>
2023-12-16 23:16:09 -0500

    day 14 pt2 (not working yet)

--- crates/day14/src/main.rs
+++ crates/day14/src/main.rs
@@ -55,6 +55,76 @@ impl Map {
         }
     }
 
+    fn tilt_south(&mut self) {
+        for x in 0..self.rows[0].len() {
+            for y in (0..self.rows.len() - 1).rev() {
+                if self.rows[y][x] == Tile::Round {
+                    let mut new_y = y;
+                    for check_y in y..self.rows.len() {
+                        if self.rows[check_y][x] == Tile::Empty {
+                            new_y = check_y;
+                        } else {
+                            break;
+                        }
+                    }
+                    if y != new_y {
+                        self.rows[new_y][x] = Tile::Round;
+                        self.rows[y][x] = Tile::Empty;
+                    }
+                }
+            }
+        }
+    }
+
+    fn tilt_west(&mut self) {
+        for y in 0..self.rows.len() {
+            for x in 0..self.rows[0].len() {
+                if self.rows[y][x] == Tile::Round {
+                    let mut new_x = x;
+                    for check_x in (0..x).rev() {
+                        if self.rows[y][check_x] == Tile::Empty {
+                            new_x = check_x;
+                        } else {
+                            break;
+                        }
+                    }
+                    if x != new_x {
+                        self.rows[y][new_x] = Tile::Round;
+                        self.rows[y][x] = Tile::Empty;
+                    }
+                }
+            }
+        }
+    }
+
+    fn tilt_east(&mut self) {
+        for y in 0..self.rows.len() {
+            for x in (0..self.rows[0].len()).rev() {
+                if self.rows[y][x] != Tile::Round && x < self.rows[0].len() - 1 {
+                    let mut new_x = x;
+                    for check_x in x..self.rows[0].len() {
+                        if self.rows[y][check_x] == Tile::Empty {
+                            new_x = check_x;
+                        } else {
+                            break;
+                        }
+                    }
+                    if x != new_x {
+                        self.rows[y][new_x] = Tile::Round;
+                        self.rows[y][x] = Tile::Empty;
+                    }
+                }
+            }
+        }
+    }
+
+    fn rotate(&mut self) {
+        self.tilt_north();
+        self.tilt_west();
+        self.tilt_south();
+        self.tilt_east();
+    }
+
     fn compute_load(&self) -> usize {
         self.rows
             .iter()
@@ -72,7 +142,17 @@ fn part1(s: &str) -> usize {
 }
 
 fn part2(s: &str) -> usize {
-    todo!()
+    let mut map = Map::from(s);
+    for _ in 0..1_000_000 {
+        let before = map.clone();
+        for _ in 0..1_000 {
+            map.rotate();
+        }
+        if map == before {
+            break;
+        }
+    }
+    map.compute_load()
 }
 
 fn main() {
@@ -98,7 +178,7 @@ O.#..O.#.#
 #....###..
 #OO..#....";
 
-    const ROLLED_NORTH: &str = "OOOO.#.O..
+    const TILTED_NORTH: &str = "OOOO.#.O..
 OO..#....#
 OO..O##..O
 O..#.OO...
@@ -109,19 +189,74 @@ O..#.OO...
 #....###..
 #....#....";
 
+    const TILTED_WEST: &str = "O....#....
+OOO.#....#
+.....##...
+OO.#OO....
+OO......#.
+O.#O...#.#
+O....#OO..
+O.........
+#....###..
+#OO..#....";
+
+    const TILTED_SOUTH: &str = ".....#....
+....#....#
+...O.##...
+...#......
+O.O....O#O
+O.#..O.#.#
+O....#....
+OO....OO..
+#OO..###..
+#OO.O#...O";
+
+    const ROTATED_ONCE: &str = ".....#....
+....#...O#
+...OO##...
+.OO#......
+.....OOO#.
+.O#...O#.#
+....O#....
+......OOOO
+#...O###..
+#..OO#....";
+
     #[test]
     fn test_part1() {
-        let mut map = Map::from(TEST_INPUT);
-        map.tilt_north();
-        let expected = Map::from(ROLLED_NORTH);
-        assert_eq!(expected, map);
         assert_eq!(part1(TEST_INPUT), 136);
     }
 
-    /*
+    #[test]
+    fn test_tilt_north() {
+        let mut map = Map::from(TEST_INPUT);
+        map.tilt_north();
+        let expected = Map::from(TILTED_NORTH);
+        assert_eq!(expected, map);
+    }
+
+    #[test]
+    fn test_tilt_west() {
+        let mut map = Map::from(TEST_INPUT);
+        map.tilt_west();
+        let expected = Map::from(TILTED_WEST);
+        assert_eq!(expected, map);
+    }
+
+    #[test]
+    fn test_tilt_south() {
+        let mut map = Map::from(TEST_INPUT);
+        map.tilt_south();
+        let expected = Map::from(TILTED_SOUTH);
+        assert_eq!(expected, map);
+    }
+
     #[test]
     fn test_part2() {
-        assert_eq!(part2(TEST_INPUT), 400);
+        let mut map = Map::from(TEST_INPUT);
+        map.rotate();
+        let expected = Map::from(ROTATED_ONCE);
+        assert_eq!(expected, map);
+        assert_eq!(part2(TEST_INPUT), 64);
     }
-    */
 }
